e2d6344ae6b365fe5d5317986139485c2fa2035e
FukayaHiroki <asitanonegif@gmail.com>
2019-08-07 17:46:47 +0900

    addDevise

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'restaurant#index'
   get 'restaurant/show' => 'restaurant#show'
+  resources :users, only: [:show]
+  resources :restaurants, only: [:index, :show, :create]
 end
