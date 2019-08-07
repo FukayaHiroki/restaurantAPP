4f186095ceff307feba5662af540ea55843b8c90
FukayaHiroki <asitanonegif@gmail.com>
2019-08-07 18:14:30 +0900

    fixController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'restaurant#index'
-  get 'restaurant/show' => 'restaurant#show'
+  root 'restaurants#index'
   resources :users, only: [:show]
   resources :restaurants, only: [:index, :show, :create]
 end
