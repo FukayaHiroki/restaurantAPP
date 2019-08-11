0bc6231fb0060600b3d94d670b8f5cdb4c8bed7f
FukayaHiroki <asitanonegif@gmail.com>
2019-08-11 11:59:09 +0900

    createJbuilder

--- app/assets/javascripts/serch.js
+++ app/assets/javascripts/serch.js
@@ -1,5 +1,40 @@
-$(document).on('turbolinks: load', function(){
-  $('#test').on('click', function(){
-    console.log('ok')
+$(document).on('turbolinks:load', function(){
+  $(function() {
+    var restaurants_list = $(".restaurants-search-result");
+
+    function appendRestaurant(restaurant) {
+      var html = `<div class="card">
+                    <div class="card-body>
+                      <h4 class="card-title">${restaurant.name}</h4> 
+                      <p class="card-text">${restaurant.content}</p>
+                      <%= link_to %>
+                    </div>
+                  </div>`
+      user_list.append(html);
+    };
+    $("#serchbox").keyup(function() {
+      var input = $("#serchbox").val();
+      $.ajax({
+      type: 'GET',
+      url: '/restaurants',
+      data: { name : input },
+      dataType: 'json'
+      })
+    .dane(function(restaurants){
+      restaurants_list.empty();
+      if (restaurants.length !== 0 && input.length !== 0) {
+        restaurants.forEach(function(restaurant) {
+          appendRestaurant(restaurant);
+        });
+      } else if (input.length == 0){
+        restaurant_list.empty();
+      } else {
+        appendNoRestaurant();
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました')
+    });
+    });
   });
-});
+})
