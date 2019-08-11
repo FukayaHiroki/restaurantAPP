b5c748ea1a40a6983cdf66847e2ceacd78353267
FukayaHiroki <asitanonegif@gmail.com>
2019-08-11 13:22:01 +0900

    updateSerch

--- app/assets/javascripts/serch.js
+++ app/assets/javascripts/serch.js
@@ -1,40 +1,40 @@
 $(document).on('turbolinks:load', function(){
   $(function() {
-    var restaurants_list = $(".restaurants-search-result");
+    var restaurants_list = $(".restaurants-serch-result");
 
     function appendRestaurant(restaurant) {
-      var html = `<div class="card">
+      var html = `<div class="card text-center">
                     <div class="card-body>
-                      <h4 class="card-title">${restaurant.name}</h4> 
+                      <h2 class="card-title"> ${restaurant.name} </h2> 
                       <p class="card-text">${restaurant.content}</p>
-                      <%= link_to %>
+                      <a href= "${restaurant.id}", class="btn btn-danger">詳細ページに行く</a>
                     </div>
                   </div>`
-      user_list.append(html);
+      restaurants_list.append(html);
     };
     $("#serchbox").keyup(function() {
       var input = $("#serchbox").val();
-      $.ajax({
+      $.ajax({ 
       type: 'GET',
-      url: '/restaurants',
+      url: '/restaurants/serch',
       data: { name : input },
       dataType: 'json'
       })
-    .dane(function(restaurants){
-      restaurants_list.empty();
-      if (restaurants.length !== 0 && input.length !== 0) {
-        restaurants.forEach(function(restaurant) {
-          appendRestaurant(restaurant);
-        });
-      } else if (input.length == 0){
-        restaurant_list.empty();
-      } else {
-        appendNoRestaurant();
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました')
-    });
+      .done(function(restaurants){
+        restaurants_list.empty();
+        if (restaurants.length !== 0 && input.length !== 0) {
+          restaurants.forEach(function(restaurant) {
+            appendRestaurant(restaurant);
+          });
+        } else if (input.length == 0){
+          restaurant_list.empty();
+        } else {
+          appendNoRestaurant();
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました')
+      });
     });
   });
 })
