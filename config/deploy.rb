246abe65eed79e29ab391af68feba90394d03b84
FukayaHiroki <asitanonegif@gmail.com>
2019-08-13 13:56:10 +0900

    tweak

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,3 @@
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
-# config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '~> 3.11.0'
 
@@ -19,7 +15,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/restaurantapp.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/restaurantapp.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
