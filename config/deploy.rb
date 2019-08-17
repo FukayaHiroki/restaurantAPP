41bd3d5a57f5fa1ece56890e5dccfc36887a470c
FukayaHiroki <asitanonegif@gmail.com>
2019-08-17 18:22:56 +0900

    tweak
    S3導入の為の微調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,8 @@ set :application, 'restaurantAPP'
 
 set :repo_url,  'git@github.com:fukayahiroki/restaurantAPP.git'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads',"config/master.key")
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
