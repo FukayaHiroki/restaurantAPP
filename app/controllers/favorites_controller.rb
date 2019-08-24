67c7d260ec7b7319e6fbc7e887631892af1d6191
FukayaHiroki <asitanonegif@gmail.com>
2019-08-24 16:45:27 +0900

    createRoutes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,21 @@
 class FavoritesController < ApplicationController
+  def create
+    @restaurant_id = Restaurant.find(params[:id]).id
+    @favorite = Favorite.new(restaurant_id: @restaurant_id, user_id: current_user.id)
+      if @clip.save
+        redirect_to user_path(current_user)
+      end
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    if favorite.destroy
+      redirect_to user_path(current_user)
+    end
+  end
+
+  private
+  def favorite_params
+    params.require(:favorite).permit(:restaurant_id).marge(:current_user.id)
+  end
 end
