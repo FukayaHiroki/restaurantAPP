6d7211b51886756a4813bd41a14075b885adfd60
FukayaHiroki <asitanonegif@gmail.com>
2019-08-25 14:10:06 +0900

    editIndexView

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   def index
     
   end
+
   def show
     @restaurants = current_user.restaurants.page(params[:page]).per(5)
+  end
+
+  def update
   end
 end
--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,10 @@
 module FavoritesHelper
+  def favorite_lists
+    html_text = ''
+    current_user.favorites.each do |favorite|
+      @restaurant = Restaurant.find(favorite.restaurant_id)
+      html_text += render(partial: 'favorite_card', locals: {restaurant: @restaurant})
+    end
+    return raw(html_text)
+  end
 end
